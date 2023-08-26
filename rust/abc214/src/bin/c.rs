af97225dd6e8a41617bd435a092db9cead037ba2
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2023-08-26 19:27:14 +0900

    add: abc214-c

--- rust/abc214/src/bin/c.rs
+++ rust/abc214/src/bin/c.rs
@@ -1,31 +1,6 @@
+use itertools::Itertools;
 use proconio::input;
-use std::collections::BinaryHeap;
-
-fn dijkstra(adj_list: Vec<Vec<(usize, i32)>>, start: usize, goal: usize) -> Option<Vec<i32>> {
-    let mut dist = vec![std::i32::MAX; adj_list.len()];
-    let mut heap = BinaryHeap::new();
-    dist[start] = 0;
-    heap.push((0, start));
-
-    while let Some((cost, u)) = heap.pop() {
-        if u == goal {
-            return Some(dist);
-        }
-
-        if dist[u] < cost {
-            // すでに最短経路が確定している
-            continue;
-        }
-        for &(v, c) in &adj_list[u] {
-            if dist[v] > dist[u] + c {
-                // 最短経路が更新できる
-                dist[v] = dist[u] + c;
-                heap.push((dist[v], v));
-            }
-        }
-    }
-    None
-}
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 fn main() {
     input! {
@@ -37,26 +12,27 @@ fn main() {
     // dbg!("{:?}", &s);
     // dbg!("{:?}", t);
 
-    // ダイクストラ法で解く
-    let mut adj_list = vec![vec![]; n + 1];
-
-    // Tから全頂点への辺を張る
-    for i in 0..n {
-        adj_list[0].push((i + 1, t[i]));
+    let mut dist = vec![std::i32::MAX; n + 1];
+    let mut heap = BinaryHeap::new();
+    let t = t.into_iter().enumerate().collect_vec();
+    for (i, t) in t {
+        heap.push((Reverse(t), i));
     }
 
-    // T以外の頂点から、その頂点以外の頂点への辺を張る
-    for i in 0..n {
-        adj_list[i + 1].push(((i + 2) % n, s[i]));
+    // O(nlogn)
+    while let Some((Reverse(cost), v)) = heap.pop() {
+        // すでに決まってる値が小さいなら更新しない
+        if dist[v] <= cost {
+            continue;
+        }
+
+        // 更新できる
+        dist[v] = cost;
+        // 次の点には最小コスト+s[v]で行ける
+        heap.push((Reverse(cost + s[v]), (v + 1) % n));
     }
 
-    dbg!(&adj_list);
-    dbg!(&adj_list.len());
-
     for i in 0..n {
-        dbg!(i);
-        let dist = dijkstra(adj_list.clone(), 0, i + 1).unwrap();
-        println!("{:?}", &dist);
         println!("{}", dist[i]);
     }
 }
